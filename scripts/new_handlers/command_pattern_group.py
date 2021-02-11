c14ee9d2ff9f86f97a5730434738d286b5e9e86b
James Lew <jamessebastianlew@gmail.com>
2021-02-11 14:21:52 +1100

    changed from type_name to type and stores the string to type converter

--- scripts/new_handlers/command_pattern_group.py
+++ scripts/new_handlers/command_pattern_group.py
@@ -1,12 +1,15 @@
+from command_pattern_constants import TYPE_SPEC_TYPES as TYPES
+from command_pattern_constants import DEFAULT_TYPE
+
 '''
 simple group class
 
 needs functionality:
 
-group = Group('var_name', static=True, type='int')
+group = Group('var_name', static=True, type=int)
 '''
 class Group:
     def __init__(self, name, static = False, type_name = ''):
         self.name = name
         self.static = static
-        self.type_name = type_name
+        self.type = TYPES[type_name] if type_name in TYPES else DEFAULT_TYPE
